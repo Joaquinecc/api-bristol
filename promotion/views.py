001cf6966a20899266982120589c110b536f7df5
joaquinecc <joaquinecc@gmail.com>
2022-02-02 11:40:47 -0300

    post array promotion

--- promotion/views.py
+++ promotion/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions
-
+from rest_framework import viewsets, permissions, status,mixins
+from rest_framework.response import Response
 from promotion import serializer
 from . import models
-class PromtionView(viewsets.ModelViewSet):
+class PromtionView(mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = models.Promotion.objects.all()
     serializer_class = serializer.PromotionSerializer
     # permission_classes = [permissions.IsAuthenticated]
-
+    def create(self, request, *args, **kwargs):
+        """
+        #checks if post request data is an array initializes serializer with many=True
+        else executes default CreateModelMixin.create function 
+        """
+        is_many = isinstance(request.data, list)
+        if not is_many:
+            return super(PromtionView, self).create(request, *args, **kwargs)
+        else:
+            serializer = self.get_serializer(data=request.data, many=True)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
