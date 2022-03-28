da56018428289f64d6540eb9db586b76d4e6f14b
joaquinecc <joaquinecc@gmail.com>
2022-03-28 17:29:31 -0400

    static path

--- apibristol/settings.py
+++ apibristol/settings.py
@@ -15,7 +15,7 @@ import os
 import json
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 try:
     filepath = os.path.join(BASE_DIR, "secrets.json")
     with open(filepath) as handle:
